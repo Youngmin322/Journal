a812aa76b0f511af9b80fb1b224795485852afcc
Youngmin Cho <choyoungmin0322@gmail.com>
2025-07-01 15:47:23 +0900

    Chore: HomeViewModel에서 JournalViewModel로 파일 이름 수정 및 클래스 이름 수정

--- Journal/Views/WirteView.swift
+++
@@ -1,8 +0,0 @@
-//
-//  WirteView.swift
-//  Journal
-//
-//  Created by 조영민 on 7/1/25.
-//
-
-import Foundation
---
+++ Journal/Views/WriteView.swift
@@ -0,0 +1,16 @@
+//
+//  WirteView.swift
+//  Journal
+//
+//  Created by 조영민 on 7/1/25.
+//
+
+import SwiftUI
+import PhotosUI
+
+struct WriteView: View {
+    @Environment(\.dismiss) private var dismiss
+    @State private var title: String = ""
+    @State private var content: String = ""
+    
+}

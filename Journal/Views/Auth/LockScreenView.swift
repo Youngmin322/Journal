a2b1df1e5ff21bc0f9d50a7a4f5736a03768adc8
Youngmin Cho <103130581+Youngmin322@users.noreply.github.com>
2025-09-03 15:02:35 +0900

    feat: LockScreenView UI 변경

--- Journal/Views/Auth/LockScreenView.swift
+++ Journal/Views/Auth/LockScreenView.swift
@@ -11,28 +11,47 @@ struct LockScreenView: View {
     @ObservedObject var authVM: AuthViewModel
     
     var body: some View {
-        VStack(spacing: 20) {
-            Image(systemName: "faceid")
-                .resizable()
-                .frame(width: 100, height: 100)
-                .foregroundColor(.blue)
+        ZStack {
+            LinearGradient(
+                gradient: Gradient(colors: [
+                    Color(red: 30/255, green: 0/255, blue: 60/255),
+                    Color(red: 10/255, green: 0/255, blue: 20/255)
+                ]),
+                startPoint: .top,
+                endPoint: .bottom
+            )
+            .ignoresSafeArea()
             
-            Text("Face ID로 잠금 해제")
-                .font(.title2)
-            
-            if let error = authVM.authError {
-                Text(error)
-                    .foregroundColor(.red)
-                    .font(.caption)
+            VStack(spacing: 20) {
+                Image(systemName: "lock.circle.fill")
+                    .resizable()
+                    .frame(width: 60, height: 60)
+                    .foregroundColor(Color("LockColor"))
+                
+                Text("일기를 보려면 Face ID를 \n 사용하십시오.")
+                    .bold()
+                    .font(.title2)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                
+                if let error = authVM.authError {
+                    Text(error)
+                        .foregroundColor(.red)
+                        .font(.caption)
+                }
+                
+                Button("잠금 해제") {
+                    authVM.authenicate()
+                }
+                .buttonStyle(.borderedProminent)
             }
-            
-            Button("잠금 해제 시도") {
+            .onAppear {
                 authVM.authenicate()
             }
-            .buttonStyle(.borderedProminent)
-        }
-        .onAppear {
-            authVM.authenicate()
         }
     }
+}
+
+#Preview {
+    LockScreenView(authVM: AuthViewModel())
 }
